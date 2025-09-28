c4ee564d72080a8d4f9ec5f114b06e181f644883
Tanush <tanub.0509@gmail.com>
2025-09-28 23:08:15 +0530

    doctor modules is working

--- src/main/java/com/hms/controller/AppointmentController.java
+++ src/main/java/com/hms/controller/AppointmentController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/appointment")
+@RequestMapping("/appointments")
 public class AppointmentController {
 
     @Autowired
--- src/main/java/com/hms/controller/DoctorController.java
+++ src/main/java/com/hms/controller/DoctorController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/doctor")
+@RequestMapping("/doctors")
 public class DoctorController {
 
     @Autowired
